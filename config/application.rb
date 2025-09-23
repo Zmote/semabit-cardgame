70eaa297c73f3b37bee5ceb6b5afa0deaf63c6db
Zafer Dogan <info@z-motions.ch>
2025-09-23 22:56:45 +0200

    feat: remove unusued engines
    - Removed Active Storage, Mailbox
    - Removed Action Text

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,17 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,7 +28,6 @@ module SemabitCardGame
     config.autoload_lib(ignore: %w[assets tasks])
 
     config.x.quotes = config_for(:quotes)
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
